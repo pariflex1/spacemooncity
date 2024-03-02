d07127f98da13de2f9f8f0a9e7c24928d2fd7c8f
pfdigital <156562295+pariflex1@users.noreply.github.com>
2024-03-02 15:23:19 +0530

    ssss

--- wp-content/plugins/elementor/assets/js/editor-loader-v1.js
+++
@@ -1,6 +0,0 @@
-/*! elementor - v3.19.0 - 07-02-2024 */
-(()=>{"use strict";var __webpack_exports__={};
-/*!*******************************************************!*\
-  !*** ../core/editor/loader/v1/js/editor-loader-v1.js ***!
-  \*******************************************************/
-window.elementor.start();})();
